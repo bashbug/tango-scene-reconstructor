a7b48228ae7d9ba1282d5b5197f998e6802716fe
Anastasia Tondera <a.tondera@gmail.com>
2015-04-23 19:11:33 +0200

    View pcl cloud with vtk refs #2

--- pcdFileReader/pcdFileReader.cpp
+++ pcdFileReader/pcdFileReader.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+
 #include <boost/thread/thread.hpp>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
+
 #include <pcl/common/common_headers.h>
 #include <pcl/range_image/range_image.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/range_image_visualizer.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/point_cloud_handlers.h>
 #include <pcl/console/parse.h>
 
 typedef pcl::PointXYZ PointType;
@@ -14,40 +15,80 @@ typedef pcl::PointXYZ PointType;
 // --------------------
 // -----Parameters-----
 // --------------------
-
-float angularResolution = (float) (  1.0f * (M_PI/180.0f));  //   1.0 degree in radians
-float maxAngleWidth     = (float) (360.0f * (M_PI/180.0f));  // 360.0 degree in radians
-float maxAngleHeight    = (float) (180.0f * (M_PI/180.0f));  // 180.0 degree in radians
-Eigen::Affine3f sensorPose = (Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f);
+float angular_resolution_x = 0.5f;
+float angular_resolution_y = angular_resolution_x;
 pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::CAMERA_FRAME;
-float noiseLevel=0.00;
-float minRange = 0.0f;
-int borderSize = 1;
+bool live_update = false;
 
-int main (int argc, char** argv)
+void setViewerPose (pcl::visualization::PCLVisualizer& viewer, const Eigen::Affine3f& viewer_pose) {
+  Eigen::Vector3f pos_vector = viewer_pose * Eigen::Vector3f(0, 0, 0);
+  Eigen::Vector3f look_at_vector = viewer_pose.rotation () * Eigen::Vector3f(0, 0, 1) + pos_vector;
+  Eigen::Vector3f up_vector = viewer_pose.rotation () * Eigen::Vector3f(0, -1, 0);
+  viewer.setCameraPosition (pos_vector[0], pos_vector[1], pos_vector[2],
+                            look_at_vector[0], look_at_vector[1], look_at_vector[2],
+                            up_vector[0], up_vector[1], up_vector[2]);
+}
+
+int
+main (int argc, char** argv)
 {
-  // pointer of point cloud object
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ> pointCloud;
 
+  // pointer of point cloud object
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>& cloud = *cloud_ptr;
   // -----------------------
   // -----Read pcd file-----
   // -----------------------
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *cloud) == -1) //* load the file
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *cloud_ptr) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
     return (-1);
   }
 
-  std::cout << "Loaded "
-            << cloud->width * cloud->height
-            << " data points from test_pcd.pcd with the following fields: "
-            << std::endl;
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-    std::cout << "    " << cloud->points[i].x
-              << " "    << cloud->points[i].y
-              << " "    << cloud->points[i].z << std::endl;
+  Eigen::Affine3f scene_sensor_pose (Eigen::Affine3f::Identity ());
+  scene_sensor_pose = Eigen::Affine3f (Eigen::Translation3f (cloud.sensor_origin_[0],
+                                                           cloud.sensor_origin_[1],
+                                                           cloud.sensor_origin_[2])) *
+                      Eigen::Affine3f (cloud.sensor_orientation_);
 
-  return (0);
 
+  // -----------------------------------------------
+  // -----Create RangeImage from the PointCloud-----
+  // -----------------------------------------------
+  angular_resolution_x = pcl::deg2rad (angular_resolution_x);
+  angular_resolution_y = pcl::deg2rad (angular_resolution_y);
+  float noise_level = 0.0;
+  float min_range = 0.0f;
+  int border_size = 1;
+  boost::shared_ptr<pcl::RangeImage> range_image_ptr(new pcl::RangeImage);
+  pcl::RangeImage& range_image = *range_image_ptr;
+  range_image.createFromPointCloud (cloud, angular_resolution_x, angular_resolution_y,
+                                    pcl::deg2rad (360.0f), pcl::deg2rad (180.0f),
+                                    scene_sensor_pose, coordinate_frame, noise_level, min_range, border_size);
+
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  pcl::visualization::PCLVisualizer viewer ("3D Viewer");
+  viewer.setBackgroundColor (1, 1, 1);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange> range_image_color_handler (range_image_ptr, 0, 0, 0);
+
+  viewer.addPointCloud (range_image_ptr, range_image_color_handler, "range image");
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "range image");
+  viewer.addCoordinateSystem ();
+
+  pcl::visualization::PointCloudColorHandlerCustom<PointType> point_cloud_color_handler (cloud_ptr, 1000, 1000, 1000);
+  viewer.addPointCloud (cloud_ptr, point_cloud_color_handler, "original point cloud");
+  viewer.initCameraParameters ();
+  setViewerPose(viewer, range_image.getTransformationToWorldSystem ());
+
+
+  //--------------------
+  // -----Main loop-----
+  //--------------------
+  while (!viewer.wasStopped ())
+  {
+    viewer.spinOnce ();
+    pcl_sleep (0.01);
+  }
 }
